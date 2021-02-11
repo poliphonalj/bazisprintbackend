169256bb387bc92f40e0a802dab3b0f2c33f23b2
gabort07 <gabort07@hotmail.com>
2021-02-11 15:10:28 +0100

    maven ground

---
+++ BazisPrint/src/main/java/Backend/BazisPrint/BazisPrintApplication.java
@@ -0,0 +1,13 @@
+package Backend.BazisPrint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BazisPrintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BazisPrintApplication.class, args);
+	}
+
+}
---
+++ BazisPrint/src/test/java/Backend/BazisPrint/BazisPrintApplicationTests.java
@@ -0,0 +1,13 @@
+package Backend.BazisPrint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BazisPrintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
